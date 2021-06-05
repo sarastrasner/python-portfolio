8d2c40779c681dc4c5e02d5e54715102615a86cc
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-05 13:35:50 -0600

    tailwind styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css';
+import '@/css/tailwind.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+require('typeface-quicksand')
 import Jumbotron from '../components/jumbotron';
 import Cards from '../components/cards'
 
