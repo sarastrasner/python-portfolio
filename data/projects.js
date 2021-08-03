c6553a2b2d840720ff507c2b88e8a790ae11e8e4
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-03 12:34:51 -0600

    day 46 image added

--- data/projects.js
+++ data/projects.js
@@ -5,6 +5,7 @@ let projectsArray = [
     description:
       "A 'Time Machine' that creates a Spotify playlist of the Billboard Top 100 songs for a user-provided date. Uses BeautifulSoup and requests for web scraping and Spotipy for authentication and playlist creation.",
     created: 'August 3, 2021',
+    image: 'spotify.png'
   },
   {
     name: 'Day 45: Movie Watchlist',
